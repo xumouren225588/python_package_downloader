457b35646031d08c0f4137acc1abcbc9a4c1f021
xumouren225588 <xumouren225588@163.com>
2025-01-18 20:06:48 +0800

    Update ra.py

--- ra.py
+++ ra.py
@@ -2,4 +2,6 @@ import random
 def generate_random_string(length):
     letters_and_digits = [chr(i) for i in range(97, 123)] + [str(i) for i in range(10)]
     return ''.join(random.choices(letters_and_digits, k=length))
-print(f"RA={generate_random_string(8)}")
+# 将环境变量写入到 $GITHUB_ENV
+with open(os.environ.get('GITHUB_ENV'), 'a') as env_file:
+    env_file.write(f"RA={generate_random_string(8)}\n")
